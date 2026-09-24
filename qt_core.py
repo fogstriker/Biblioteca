a882d51d2f43be8e3edcdd5fb11113b7d633c745
fogstriker <123199273+fogstriker@users.noreply.github.com>
2026-09-24 16:00:14 -0300

    QT_core ajuste

--- qt_core.py
+++ qt_core.py
@@ -1,8 +1,7 @@
 
 
-from qt_core import *
-
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
+from PySide6.QtCore import QRect, QSize, Qt
+from pySide6.QtGui import QFont, QIcon, QPixmap, QAction
+from PySide6.QtWidgets import  QStackedWidget, QMenuBar, QHBoxLayout, QFrame, QVBoxLayout, QWidget, QPushButton, QGridLayout, QMainWindow, QLabel
+from PySide6 import QtCore, QtWidgets
 
